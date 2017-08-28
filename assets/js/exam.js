411c3252be234fb36ed8d387b187a11f8f2fe6d1
bachnx <xubaxabu@gmail.com>
2017-08-28 16:58:24 +0700

    done - send client v4

--- assets/js/exam.js
+++ assets/js/exam.js
@@ -141,5 +141,16 @@ function force_user(){
 
 function show_modal(message){
   // $('#doneModal').modal('show');
-
 }
+
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
